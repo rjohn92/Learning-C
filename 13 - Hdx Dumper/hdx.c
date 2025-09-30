b08e6ddb3be7f5d3a8fa41f2da8dd6195ea664e8
rjohn92 <rolandjohn92@gmail.com>
2025-09-29 22:31:50 -0400

    edits the hdx.c

--- 13 - Hdx Dumper/hdx.c
+++ 13 - Hdx Dumper/hdx.c
@@ -2,22 +2,46 @@
 
 // accept optional filename
 int main(int argc, char *argv[]) {
-    char *path;
-    FILE *fp = stdin;
 
-    if (argc > 1 && argv[1][0] != '-') {
-        printf("File opened successfully.\n");
-        fp = fopen(argv[1], "rb");
+    // If a filename is provided as an argument, use it; otherwise, use NULL for stdin
+    const char *path = (argc > 1) ? argv[1]: NULL;
+    FILE *fp = NULL;
+    unsigned char buf[16];
+    unsigned int offset = 0;
+
+    // open file or use stdin
+    if (path) {
+        fp = fopen(path, "rb");
         if (!fp) {
             perror("fopen");
             return 1;
         }
-        path = argv[1];
+
     } else {
-        printf("No file provided.\n");
-        return 1;
+        fp = stdin;
     }
+
+    unsigned int got = fread(buf, 1, sizeof buf, fp);
+    while (got > 0) {
+        printf("Offset %08X: ", offset);
+        for (size_t i = 0; i < got; i++) {
+            printf("%02X ", buf[i]);
         }
+        printf("\n");
+
+        offset += got;
+        if (got < sizeof buf) {
+            break;
+        }
+        got = fread(buf, 1, sizeof buf, fp);
+    }
+
+    if (fp != stdin) {
+        fclose(fp);
+    }
+    return 0;
+}
+
 
 
 
